c7d1a95e357ee5ee59b288a0ced0317bc3fe572c
Nithyanandhan Velliyangiri <46887333+NickolusAlex@users.noreply.github.com>
2025-08-08 00:15:52 +0530

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -43,7 +43,8 @@ export default defineConfig({
         launchOptions: {
           args: ['--start-maximized']
         },
-        headless: false,
+        // headless: false,
+        headless: true,
         viewport: null
       },
     },
